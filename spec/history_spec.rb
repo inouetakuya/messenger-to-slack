9770794d5c77a906a78c53ef625b3d01949bdbc3
INOUE Takuya <inouetakuya5@gmail.com>
2018-09-04 01:29:12 +0900

    + it 'exports messages to CSV files'

--- spec/history_spec.rb
+++ spec/history_spec.rb
@@ -1,4 +1,20 @@
 describe History do
+  describe '.export_csv' do
+    after do
+      FileUtils.rm_rf(Dir[File.join(History.dist_dir, '*')])
+    end
+
+    it 'exports messages to CSV files' do
+      expect {
+        History.export_csv
+      }.to change { Dir[File.join(History.dist_dir, '**/messages.csv')].size }
+    end
+
+    it 'returns messages count' do
+      expect(History.export_csv).to be > 0
+    end
+  end
+
   describe '.parse_json' do
     it 'message.json からメッセージ履歴を取得できる' do
       expect(History.parse_json['messages']).to be_present
--- src/history.rb
+++ src/history.rb
@@ -1,9 +1,54 @@
+require 'csv'
 require 'json'
 
 class History
   class << self
+    def export_csv(channel_name: 'general')
+      current = Time.current
+
+      FileUtils.mkdir_p(csv_dir(current: current))
+
+      CSV.open(csv_path(current: current), 'w') do |csv|
+        parse_json['messages'].each do |data|
+          message = Message.new(
+            sender_name: data['sender_name'],
+            timestamp_ms: data['timestamp_ms'],
+            content: data['content'],
+          )
+
+          if message.content.present?
+            csv << [message.timestamp, channel_name, message.sender_name, message.content]
+          end
+        end
+      end
+
+      parse_json['messages'].size
+
+    rescue => exception
+      FileUtils.rm_rf(csv_dir(current: current)) if Dir.exist?(csv_dir(current: current))
+      raise exception
+    end
+
     def parse_json
-      JSON.parse(File.open(json_path).read)
+      @json_data ||= JSON.parse(File.open(json_path).read)
+    end
+
+    def dist_dir
+      if ENV['ENV'] == 'test'
+        File.join(MessengerToSlack::ROOT_PATH, 'spec/dist')
+      else
+        File.join(MessengerToSlack::ROOT_PATH, 'dist')
+      end
+    end
+
+    private
+
+    def csv_dir(current: Time.current)
+      File.join(dist_dir, current.strftime('%Y%m%d%H%M%S'))
+    end
+
+    def csv_path(current: Time.current)
+      File.join(csv_dir(current: current), 'messages.csv')
     end
 
     def json_path
