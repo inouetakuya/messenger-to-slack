a8adf86f3c19738c9fa83b149bd4779c5ba9af37
INOUE Takuya <inouetakuya5@gmail.com>
2018-09-04 23:25:48 +0900

    rescue Mechanize::ResponseCodeError している箇所について、理由をコメントで残した

--- src/message.rb
+++ src/message.rb
@@ -33,12 +33,6 @@ class Message
     Message.iso88591_to_utf8(@content) unless @content.nil?
   end
 
-  def convert_to_roman(name)
-    name.to_kanhira.to_roman
-  rescue Mechanize::ResponseCodeError
-    name.to_roman
-  end
-
   def validate_sender_name!
     if sender_name.blank?
       raise'sender_name が入っていません'
@@ -49,5 +43,29 @@ class Message
     end
 
     true
+  end
+
+  private
+
+  def convert_to_roman(name)
+    name.to_kanhira.to_roman
+
+  rescue Mechanize::ResponseCodeError
+    # pry(main)> "いのうえ たくや".to_kanhira
+    #
+    # Mechanize::ResponseCodeError: 403 => Net::HTTPForbidden for https://yomikatawa.com/kanji/%E3%81%84%E3%81%AE%E3%81%86%E3%81%88%20%E3%81%9F%E3%81%8F%E3%82%84 -- unhandled response
+    # from /Users/inouetakuya/src/github.com/inouetakuya/messenger-to-slack/vendor/bundle/ruby/2.5.0/gems/mechanize-2.7.5/lib/mechanize/http/agent.rb:323:in `fetch'
+    #
+    # となるので is_kanji? メソッドで判定してから to_kanhira を使おうと考えたが、
+    #
+    # pry(main)> "井上 たくや".is_kanji?
+    # => false
+    #
+    # pry(main)> "井上 たくや".to_roman
+    # => "takuya"
+    #
+    # となるため、やむを得ず例外キャッチ
+
+    name.to_roman
   end
 end
